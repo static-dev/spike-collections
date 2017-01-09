fef9b6a63c8ace12eb994b6004ed11ce6561398a
Jeff Escalante <escalantejeff@gmail.com>
2017-01-09 16:35:06 -0500

    adjust transform function to fit spec

--- test/index.js
+++ test/index.js
@@ -42,7 +42,7 @@ test.cb('pagination', (t) => {
     collections: {
       posts: {
         files: 'posts/**',
-        transform: (p) => { return { permalink: 'extra!' } },
+        transform: (l) => { return Object.assign(l, { permalink: 'extra!' }) },
         paginate: {
           template: 'posts/_template.html',
           perPage: 1,
@@ -59,18 +59,18 @@ test.cb('pagination', (t) => {
     const post1 = fs.readFileSync(path.join(publicPath, 'posts/foo.html'), 'utf8')
     const post2 = fs.readFileSync(path.join(publicPath, 'posts/bar.html'), 'utf8')
     t.is(post1.trim(), '<p>hello amaze!</p>')
-    t.is(post2.trim(), '<locals>snargle!</locals>\n<permalink>extra!</permalink>\n<script>{"posts":[{"dingle":"snargle","permalink":"extra!","_path":"posts/bar.html"},{"wow":"amaze","permalink":"extra!","_path":"posts/foo.html"}]}</script>')
+    t.is(post2.trim(), '<locals>snargle!</locals>\n<permalink>extra!</permalink>\n<script>{"posts":[{"dingle":"snargle","_path":"posts/bar.html","_collection":"posts","permalink":"extra!"},{"wow":"amaze","_path":"posts/foo.html","_collection":"posts","permalink":"extra!"}]}</script>')
 
     // pages 1 and 2 are present and contain the right locals
     const page1 = fs.readFileSync(path.join(publicPath, 'posts/p1.html'), 'utf8')
     const page2 = fs.readFileSync(path.join(publicPath, 'posts/p2.html'), 'utf8')
 
-    t.is(page1.trim(), '<pages>{"posts":[{"page":1,"path":"posts/p1.html","posts":[{"dingle":"snargle","permalink":"extra!","_path":"posts/bar.html"}]},{"page":2,"path":"posts/p2.html","posts":[{"wow":"amaze","permalink":"extra!","_path":"posts/foo.html"}]}]}</pages>\n<current>{"page":1,"path":"posts/p1.html","posts":[{"dingle":"snargle","permalink":"extra!","_path":"posts/bar.html"}]}</current>')
-    t.is(page2.trim(), '<pages>{"posts":[{"page":1,"path":"posts/p1.html","posts":[{"dingle":"snargle","permalink":"extra!","_path":"posts/bar.html"}]},{"page":2,"path":"posts/p2.html","posts":[{"wow":"amaze","permalink":"extra!","_path":"posts/foo.html"}]}]}</pages>\n<current>{"page":2,"path":"posts/p2.html","posts":[{"wow":"amaze","permalink":"extra!","_path":"posts/foo.html"}]}</current>')
+    t.is(page1.trim(), '<pages>{"posts":[{"page":1,"path":"posts/p1.html","posts":[{"dingle":"snargle","_path":"posts/bar.html","_collection":"posts","permalink":"extra!"}]},{"page":2,"path":"posts/p2.html","posts":[{"wow":"amaze","_path":"posts/foo.html","_collection":"posts","permalink":"extra!"}]}]}</pages>\n<current>{"page":1,"path":"posts/p1.html","posts":[{"dingle":"snargle","_path":"posts/bar.html","_collection":"posts","permalink":"extra!"}]}</current>')
+    t.is(page2.trim(), '<pages>{"posts":[{"page":1,"path":"posts/p1.html","posts":[{"dingle":"snargle","_path":"posts/bar.html","_collection":"posts","permalink":"extra!"}]},{"page":2,"path":"posts/p2.html","posts":[{"wow":"amaze","_path":"posts/foo.html","_collection":"posts","permalink":"extra!"}]}]}</pages>\n<current>{"page":2,"path":"posts/p2.html","posts":[{"wow":"amaze","_path":"posts/foo.html","_collection":"posts","permalink":"extra!"}]}</current>')
 
     // index is there and contains the right locals
     const index = fs.readFileSync(path.join(publicPath, 'index.html'), 'utf8')
-    t.is(index.trim(), '<all-posts>{"posts":[{"dingle":"snargle","permalink":"extra!","_path":"posts/bar.html"},{"wow":"amaze","permalink":"extra!","_path":"posts/foo.html"}]}</all-posts>')
+    t.is(index.trim(), '<all-posts>{"posts":[{"dingle":"snargle","_path":"posts/bar.html","_collection":"posts","permalink":"extra!"},{"wow":"amaze","_path":"posts/foo.html","_collection":"posts","permalink":"extra!"}]}</all-posts>')
 
     rimraf.sync(publicPath)
     t.end()
@@ -89,12 +89,12 @@ test.cb('collection without pagination', (t) => {
     const publicPath = path.join(root, 'public')
 
     const index = fs.readFileSync(path.join(publicPath, 'index.html'), 'utf8')
-    t.is(index, '<all-posts>{"posts":[{"dingle":"snargle","_path":"posts/bar.html"},{"wow":"amaze","_path":"posts/foo.html"}]}</all-posts>\n')
+    t.is(index, '<all-posts>{"posts":[{"dingle":"snargle","_path":"posts/bar.html","_collection":"posts"},{"wow":"amaze","_path":"posts/foo.html","_collection":"posts"}]}</all-posts>\n')
 
     const post1 = fs.readFileSync(path.join(publicPath, 'posts/bar.html'), 'utf8')
     const post2 = fs.readFileSync(path.join(publicPath, 'posts/foo.html'), 'utf8')
 
-    t.is(post1, '<locals>snargle!</locals>\n<script>{"posts":[{"dingle":"snargle","_path":"posts/bar.html"},{"wow":"amaze","_path":"posts/foo.html"}]}</script>\n')
+    t.is(post1, '<locals>snargle!</locals>\n<script>{"posts":[{"dingle":"snargle","_path":"posts/bar.html","_collection":"posts"},{"wow":"amaze","_path":"posts/foo.html","_collection":"posts"}]}</script>\n')
     t.is(post2, '<p>hello amaze!</p>\n')
 
     rimraf.sync(publicPath)
